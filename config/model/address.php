d2131a8a1698b1f4be7453896553915271617abf
wzj <wzj@33d85105-05b1-b34f-9e97-49e1157588f9>
2019-12-28 07:15:23 +0000

    git-svn-id: https://39.108.144.9:8443/svn/bink@52 33d85105-05b1-b34f-9e97-49e1157588f9

--- config/model/address.php
+++ config/model/address.php
@@ -9,7 +9,7 @@ return [
     /*
      * Modules.
      */
-    'modules'  => ['city','province'],
+    'modules'  => ['city','province','area'],
 
     'policies' => [
 
@@ -71,6 +71,23 @@ return [
         'perPage'       => '20',
         'search'        => [
             'name'        => 'like',
+        ],
+    ],
+    'area'     => [
+        'model'        => 'App\Models\Area',
+        'table'        => 'areas',
+        'primaryKey'   => 'id',
+        'hidden'       => [],
+        'visible'      => [],
+        'guarded'      => ['*'],
+        'fillable'     => ['code', 'parent_code', 'remark_two'],
+        'translate'    => [''],
+        'upload_folder' => 'region',
+        'encrypt'      => ['id'],
+        'revision'     => [],
+        'perPage'      => '20',
+        'search'        => [
+            'title'  => 'like',
         ],
     ],
 ];
